bed3e84aab0daeb3b4c6d1d5264ba3e42ce584be
hmrp <hmrp@users.noreply.github.com>
2024-04-01 12:02:58 +0100

    Add files via upload

--- abola/videoIMA/script.js
+++ abola/videoIMA/script.js
@@ -8,7 +8,7 @@ window.Vlitejs.registerPlugin('sticky', window.VlitejsSticky, {
 
 window.Vlitejs.registerPlugin("ima", window.VlitejsIma, {
   adTagUrl:
-    "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/vmap_ad_samples&sz=640x480&cust_params=sample_ar%3Dpreonlybumper&ciu_szs=300x250&gdfp_req=1&ad_rule=1&output=vmap&unviewed_position_start=1&env=vp&impl=s&correlator=",
+    "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_ad_samples&sz=640x480&cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=",
   updateImaSettings: (imaSettings) => {
     imaSettings.setLocale('pt');
   },
